efa082f2329bc9c54be4e2113eebd52575531ad9
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-09-04 14:21:45 +0530

    Add Header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,11 +1,18 @@
 import Head from "next/head";
 
+import Header from "../components/Header";
+
 export default function Home() {
   return (
     <>
       <Head>
         <title>Home</title>
       </Head>
+      <main>
+        <div className="max-w-wrapper px-4 bg-blue-600">
+          <Header />
+        </div>
+      </main>
     </>
   );
 }
