6ab0fa1e4818271b7cd5cd36a24564c8e5a5d870
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-09-10 17:37:26 +0530

    Make cards in / route clickable (#5)

--- src/components/country/Country.jsx
+++ src/components/country/Country.jsx
@@ -1,35 +1,38 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Country({ data }) {
   return (
-    <li
-      key={data.name.official}
-      className="grid grid-rows-[10rem,_minmax(176px,_auto)] overflow-hidden rounded-lg bg-blue-600"
-    >
-      <Image
-        src={data.flags.svg}
-        alt={`Flag of ${data.name.official}`}
-        width={264}
-        height={160}
-        loading="lazy"
-        className="self-stretch object-cover object-center"
-      />
-      <div className="p-6">
-        <h3 className="mb-4 text-lg font-bold leading-3">
-          {data.name.official}
-        </h3>
-        <div className="flex flex-col gap-y-2">
-          <p className="text-sm leading-1">
-            <span className="font-medium">Population</span>: {data.population}
-          </p>
-          <p>
-            <span className="font-medium">Region</span>: {data.region}
-          </p>
-          <p>
-            <span className="font-medium">Capital</span>: {data.capital}
-          </p>
+    <li key={data.name.official}>
+      <Link
+        href={`/country/${data.cca3.toLowerCase()}`}
+        className="grid h-full grid-rows-[10rem,_minmax(176px,_auto)] overflow-hidden rounded-lg bg-blue-600 transition-all hover:scale-105"
+      >
+        <Image
+          src={data.flags.svg}
+          alt={`Flag of ${data.name.official}`}
+          width={264}
+          height={160}
+          loading="lazy"
+          className="self-stretch object-cover object-center transition-all"
+        />
+        <div className="p-6">
+          <h3 className="mb-4 text-lg font-bold leading-3">
+            {data.name.official}
+          </h3>
+          <div className="flex flex-col gap-y-2">
+            <p className="text-sm leading-1">
+              <span className="font-medium">Population</span>: {data.population}
+            </p>
+            <p>
+              <span className="font-medium">Region</span>: {data.region}
+            </p>
+            <p>
+              <span className="font-medium">Capital</span>: {data.capital}
+            </p>
+          </div>
         </div>
-      </div>
+      </Link>
     </li>
   );
 }
