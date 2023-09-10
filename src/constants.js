dca7c4d0dd09eeb1f70d9fc1e895f887c8342810
Abhijith Muthyala <73361366+abhijithmuthyala@users.noreply.github.com>
2023-09-10 17:36:24 +0530

    Make cards in / route clickable

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 const baseURL = "https://restcountries.com/v3.1";
 
 export const API_URL = {
-  all: `${baseURL}/all?fields=name,flags,population,region,capital`,
+  all: `${baseURL}/all?fields=name,flags,population,region,capital,cca3`,
   countries: `${baseURL}/all?fields=cca3`,
   country(code) {
     return `${baseURL}/alpha/${code}?fields=name,nativeName,population,region,subregion,capital,topLevelDomain,currencies,languages,borders,flags,cca3`;
